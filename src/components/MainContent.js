fe5c8a8049cc6289d0743f3a39b280f29b5d12a1
Sara Simon <sara.simon.10.07@gmail.com>
2021-04-21 20:10:17 -0700

    Version a million

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,13 +1,10 @@
 import React, { useEffect } from "react"
 
-import sgkochiimg from "../public/SaharaGraceKochi.jpg"
-
 function MainContent() {
     return (
         <>
             <div className="container">
-                <br />
-                <img src={sgkochiimg} />
+                <h1>Hello!</h1>
             </div>
         </>
     )
